15f56f0068444ebd80a73876da136f9f0e112ff3
Anna Yudina <anya.yud@gmail.com>
2019-09-04 19:06:27 +0300

    Add files via upload

---
+++ two_sum.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+#Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+def twoSum(nums: List[int], target: int):
+
+    lst = []
+    for el in range(len(nums)):
+        for elem in range(el+1,len(nums)):
+            if nums[el] + nums[elem] == target:
+                lst.append(el)
+                lst.append(elem)
+        if lst==[]:
+            continue
+        else: 
+            break
+
+    return lst
+
