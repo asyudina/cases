72f35b80ac05450f0b2b32e69f9fa1d7994e8215
Anna Yudina <anya.yud@gmail.com>
2019-09-04 19:00:08 +0300

    Add files via upload

---
+++ Palindrome Number.py
@@ -0,0 +1,31 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+def isPalindrome(x: int):
+
+    if x<0:
+        return False 
+    elif x ==0:
+        return True 
+    else:
+        lst1 = [int(d) for d in str(x)]
+        lst2  = []
+        if len(lst1) == 1:
+            return x
+        cur_pos = 0 
+        for el in range(len(lst1)-1,-1,-1):
+            if lst1[el] == lst1[el-el+cur_pos]:
+                lst2.append(lst1[el])
+                cur_pos +=1
+            else:
+                return False
+    try:         
+        poly = int("".join(list(map(str,lst2))))
+    except:
+        return False
+
+    return poly
+
